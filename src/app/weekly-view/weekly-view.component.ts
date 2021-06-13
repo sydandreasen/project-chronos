e1c292358e3f424533a0abf954fb56dcb76f7445
sydandreasen <sydandreasen@gmail.com>
2021-06-13 17:22:10 -0500

    prepare weekly for date logic

--- src/app/weekly-view/weekly-view.component.ts
+++ src/app/weekly-view/weekly-view.component.ts
@@ -3,13 +3,36 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-weekly-view',
   templateUrl: './weekly-view.component.html',
-  styleUrls: ['./weekly-view.component.scss']
+  styleUrls: ['./weekly-view.component.scss'],
 })
 export class WeeklyViewComponent implements OnInit {
+  focusDate = new Date(); // default date to build the week around.
+  weekDates: Date[] = []; // array of dates to populate UI
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    // TODO Royal to implement logic around dates
+    // i.e. based on the current date, pull the day of week, and grab
+    // dates for start and end of week accordingly
+    // set those into this.weekDates arrray
+    // after doing so, you should see the date numbers populate in the UI..
+    // because you will also need to find list of dates any time the navigation buttons are clicked,
+    // a function for this based on this.focusDate would be good instead of repeating code in
+    // nextWeek() and lastWeek() functions
   }
 
+  // navigate to next week
+  nextWeek() {
+    console.log('navigate to next week');
+    // TODO Royal to implement
+    // should effect this.focusDate and this.weekDates
+  }
+
+  // navigate to previous week
+  lastWeek() {
+    console.log('navigate to previous week');
+    // TODO Royal to implement
+    // should effect this.focusDate and this.weekDates
+  }
 }
