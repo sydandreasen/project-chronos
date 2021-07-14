1e22db308e97ba868453f089161865c651dd3385
Tendopolis <84944944+Tendopolis@users.noreply.github.com>
2021-07-14 04:58:10 -0500

    SRC of working color changes
    Cant figure out how to put all my changes here so im trying different things to make it work.

---
+++ app/components/daily-view/daily-view.component.ts
@@ -0,0 +1,79 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+/**
+ * shows/manages daily view of plan endpoint
+ */
+@Component({
+  selector: 'app-daily-view',
+  templateUrl: './daily-view.component.html',
+  styleUrls: ['./daily-view.component.scss'],
+})
+export class DailyViewComponent {
+  /** the currently shown date */
+  @Input() focusDate: Date = new Date();
+
+  @Input() chosenColor: String = '';
+
+  /** set focus date back at top to communicate between */
+  @Output() sendFocusDate: EventEmitter<Date> = new EventEmitter<Date>();
+
+  slides: { image: string }[] = [];
+
+  ngOnInit(): void {
+    // test a couple images for the carousel
+    this.slides.push({
+      image:
+        'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
+    });
+    this.slides.push({
+      image:
+        'https://cdn.pixabay.com/photo/2014/09/14/18/04/dandelion-445228__340.jpg',
+    });
+    this.slides.push({
+      image:
+        'https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832__340.jpg',
+    });
+    this.slides.push({
+      image:
+        'https://cdn.pixabay.com/photo/2015/06/19/21/24/avenue-815297__340.jpg',
+    });
+  }
+
+  /**
+   * reset the focused date back to today
+   */
+  jumpToToday() {
+    this.setFocusDate(new Date());
+  }
+
+  /** jump to a new focus date.
+   * @param date the date to now focus on
+   */
+  setFocusDate(date: Date): void {
+    this.sendFocusDate.emit(date);
+  }
+
+  /**
+   * navigate to tomorrow (relative to focusDate)
+   */
+  tomorrow() {
+    let temp: Date = new Date(
+      this.focusDate.getFullYear(),
+      this.focusDate.getMonth(),
+      this.focusDate.getDate() + 1
+    );
+    this.setFocusDate(temp);
+  }
+
+  /**
+   * navigate to yesterday (relative to focusDate)
+   */
+  yesterday() {
+    let temp: Date = new Date(
+      this.focusDate.getFullYear(),
+      this.focusDate.getMonth(),
+      this.focusDate.getDate() - 1
+    );
+    this.setFocusDate(temp);
+  }
+}
---
+++ environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAIk2NqaNJlbd74RYiT-kUKR-I_zhq_vcQ',
+    authDomain: 'project-chronos-planner.firebaseapp.com',
+    databaseURL: 'https://project-chronos-planner-default-rtdb.firebaseio.com',
+    projectId: 'project-chronos-planner',
+    storageBucket: 'project-chronos-planner.appspot.com',
+    messagingSenderId: '167280902326',
+    appId: '1:167280902326:web:b6ecb6e5883ed7a66a2617',
+    measurementId: 'G-6DG7L5XKW7',
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
